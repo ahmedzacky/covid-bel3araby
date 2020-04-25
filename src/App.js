658aa240cc3682d636442ad4545af2dcc9244fa1
ahmedzacky <ahmazaki97@gmail.com>
2020-04-25 23:57:28 +0200

    same app rewritten completly in hooks

--- src/App.js
+++ src/App.js
@@ -1,83 +1,83 @@
-import React, {Component} from "react"
+import React, { useState, useEffect } from "react"
 import Axios from "axios"
 import ArCountries from './countries/countries.json'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faGithub} from '@fortawesome/free-brands-svg-icons'
 import { Confirmed, Active, Recovered, DeathRatio, Deaths } from './components/Stats'
 import Disclaimer from "./components/Disclaimer";
 import Cards from "./components/Cards";
 import SearchCountries from "./components/SearchCountries"
+import Head from "./components/Head";
 
-export default class App extends Component{
-    constructor(){
-        super();
-            this.state = {
-                countryData: {updated:0,cases:0, todayCases:0, deaths:0, todayDeaths:0, recovered:0, active:0},
-                Arabized: []
-            }
-        this.getCountryData = this.getCountryData.bind(this)
-    }
 
-    componentDidMount(){ this.getData(); this.setArabized() }
+const App = () => {
+    const [countryData, setcountryData] = useState({updated:0,cases:0, todayCases:0, deaths:0, todayDeaths:0, recovered:0, active:0});
+    const [Arabized, setArabized] = useState([]);
 
-    /* Setting worldwide (default) state*/
-     async getData(){
-        const resApi = await Axios.get('https://corona.lmao.ninja/v2/all')
-        this.setState({ countryData : resApi.data })
-    }
-    
-    /*Arabizing country names*/
-    async setArabized(){
-        const resCountries = await Axios.get("https://corona.lmao.ninja/v2/countries?sort=deaths");
-        const countries = [];
+    const getGlobalData = async () => {
+        const resApi = await Axios.get('https://corona.lmao.ninja/v2/all');
+        setcountryData(resApi.data);
+    };
+
+    const fetchAndArabize = async() =>{
+        const response = await Axios.get("https://corona.lmao.ninja/v2/countries?sort=deaths");
+        const countries= [];
         const Arabized= [];
-
+    
         /* Importing ISO2 country names */
-        for(let k = 0; k < resCountries.data.length; k++){
-            if (resCountries.data[k].countryInfo.iso2){
-                countries.push(resCountries.data[k].countryInfo.iso2);
+        for(let k = 0; k < response.data.length; k++){
+            if (response.data[k].countryInfo.iso2){
+                countries.push(response.data[k].countryInfo.iso2);
             }
         }
-
+    
         /* Arabization of countries as options */
         for(let j=0; j<countries.length;j++){
-            for(let i=0; i<ArCountries.arcountries.length;i++){
+            for(let i=0; i<ArCountries.arcountries.length; i++){
                 if(ArCountries.arcountries[i].code === countries[j]){
                 Arabized.push({value: countries[j], label: ArCountries.arcountries[i].name})
                 }
             }
         }
-        this.setState({ Arabized })
+        setArabized(Arabized)
+    }; 
+    
+
+    const initialEffect = () => { 
+        getGlobalData();
+        fetchAndArabize(); 
     }
 
+    useEffect(() => initialEffect() , [])
 
-    /* Setting state for chosen country*/
-    async getCountryData(event){
-        if (!event) return this.getData()
+    
+    const getCountryData = async (event) => {
+        if(event){
         const res = await Axios.get(`https://corona.lmao.ninja/v2/countries/${event.value}`)
-        this.setState({ countryData : res.data })
+        setcountryData(res.data)
+        } else {
+            getGlobalData()
+        }
     }
 
+    let deathRatio = 0
+    deathRatio = countryData.deaths && (countryData.deaths / countryData.cases) * 100 ;
 
-    /* Rendering main App*/
-    render(){
-        let deathRatio = 0
-        deathRatio = this.state.countryData.deaths && (this.state.countryData.deaths / this.state.countryData.cases) * 100 ;
-        return (
+    return (
+        
         <div className="container">
-            <a href="https://github.com/ahmedzacky/covid-bel3araby" className="github"><FontAwesomeIcon icon={faGithub} /></a>
-            <h1> اخر احصائيات فيروس الكورونا</h1>
-            <SearchCountries countryData={this.state.countryData} Arabized={this.state.Arabized} getCountryData={this.getCountryData} />
+            <br></br>
+            <Head/>
+            <SearchCountries countryData={countryData} Arabized={Arabized} getCountryData={getCountryData}/>
             <div className='flex'>
-                <Confirmed cases={this.state.countryData.cases} todayCases={this.state.countryData.todayCases} />
-                <Active active={this.state.countryData.active} />
-                <Recovered recovered={this.state.countryData.recovered} />
+                <Confirmed cases={countryData.cases} todayCases={countryData.todayCases} />
+                <Active active={countryData.active} />
+                <Recovered recovered={countryData.recovered} />
                 <DeathRatio deathRatio={deathRatio} />
-                <Deaths deaths={this.state.countryData.deaths} todayDeaths={this.state.countryData.todayDeaths} />
+                <Deaths deaths={countryData.deaths} todayDeaths={countryData.todayDeaths} />
             </div>
-            <Disclaimer updated={this.state.countryData.updated} /> 
+            <Disclaimer updated={countryData.updated} /> 
             <Cards />     
         </div>
         )
-    }
-} 
+}
+
+export default App
